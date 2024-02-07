e4f518d957e5153ba9f680c35d922bf5175b7eb2
salvatore_barbaro <salvatore.ilbarbaro97@gmail.com>
2024-02-07 23:49:06 +0100

    creazione ciclo della funzione

--- palindromi/code/codice.js
+++ palindromi/code/codice.js
@@ -5,10 +5,45 @@
 
 
 // Chiedere all’utente di inserire una parola.
-    // -inserisco parola in un array
+    // -inserisco parola in una const
 
 // Creare una funzione per capire se la parola inserita è palindroma
         // -funzione che partira dal fondo e copiera la parola al contrario
 
 
-const words=[];
+document.querySelector("button").addEventListener("click",
+function()
+{
+    // dichiarazione parola utente
+    const userword = document.getElementById("userwords").value;
+    console.log(userword);
+    
+
+
+}
+)
+
+
+
+function iswordpalindrome(word)
+{
+    let invsertword=0
+
+    // i=lunghezza parola, i maggiore uguale di 0 e sarà i-- perchè sto andando a ritroso nella parola
+    for(i=length.word;i>=0;i--)
+    {
+        // inserisco carattere in invertword uguale al carattere su cui è la i nel ciclo sulla mia parola 
+        invsertword += word[i];
+
+    }
+
+    if(invsertword==word)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
